6818051ff05add5a290ae18ef809e76330959cde
klokov <dnwjdgus1994@gmail.com>
2020-03-22 11:46:33 +0900

    scraper.py test

--- Modules/Scraper/scraper.py
+++ Modules/Scraper/scraper.py
@@ -13,17 +13,19 @@ from openpyxl import load_workbook
 
  
 #지정 경로에 있는 엑셀 파일 읽어오기
-load_wb = load_workbook("test.xlsx", data_only=True)
+load_wb = load_workbook("../../test.xlsx")
 #특정 시트 사용하기
 load_ws = load_wb['Schedules']
  
 #스케쥴 이름 및 기본 정보 
-print('Schedule Name:'+load_ws['E5'].value+"\n")
-print('Author:'+load_ws['S5'].value)
-print('Training per week:'+str(load_ws['S6'].value))
-print('Weight unit:'+load_ws['S7'].value+"\n")
+print("")
+print('Schedule Name     : '+load_ws['E5'].value+"\n")
+print('Author            : '+load_ws['S5'].value)
+print('Training per week : '+str(load_ws['S6'].value))
+print('Weight unit       : '+load_ws['S7'].value+"\n")
 
 #Lifting Category Table
+'''
 multiple_cells = load_ws['R9':'U17']
 for row in multiple_cells:
     print("")
@@ -32,6 +34,18 @@ for row in multiple_cells:
          break
         else:
          print(cell.value,end='\t\t')
+'''
+multiple_cells = load_ws['R9':'S17']
+for row in multiple_cells :
+    print("")
+    for cell in row :
+        if cell.value==None:
+            break
+        else:
+            print('%-22s'%cell.value,end='\t')
+
+
+
 
 #Assistance Exercies table
 multiple_cells = load_ws['R19':'S25']
@@ -42,10 +56,15 @@ for row in multiple_cells:
          break
         else:
          print(cell.value,end='\t\t')
+print("")
+
 
 #Main Schedule table        
 multiple_cells = load_ws['B9':'P117']
 for row in multiple_cells:
     print("")
     for cell in row:
-       print(cell.value,end='\t')
+        if cell.value is None :
+            pass
+        else:
+            print(cell.value,end='\t')
