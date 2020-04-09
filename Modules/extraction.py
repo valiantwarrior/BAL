0de6e06bfbb2d9346c67f300fb27736e5d5ed49e
YuJin <susanna1221@naver.com>
2020-04-10 00:24:20 +0900

    update extraction.py

--- Modules/extraction.py
+++ Modules/extraction.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 '''
 import Modules.Scraper.bal_excel_scraper as Scraper
 import Modules.Classes.bal_daily_routine as dr
@@ -13,11 +12,13 @@ from Modules.Classes import bal_workout_program as wp
 
 EXCEL_SHEET_DIR = "../test.xlsx"
 
-num_lifting_category = len(Scraper.bal_get_lifting_category(sheet))
+
 
 def init_workout_program() :
     
     sheet = Scraper.bal_load_excel(EXCEL_SHEET_DIR)
+    num_lifting_category = len(Scraper.bal_get_lifting_category(sheet))
+   
     
     workout_program = wp.BALWorkoutProgram(
         Scraper.bal_get_author(sheet),
@@ -29,6 +30,7 @@ def init_workout_program() :
     workout_program.training_day = Scraper.bal_get_training_days(sheet)
 
     index = 0
+    workout_program_data=[]
     while index < len(workout_program.training_day) :
         workout_program_data.append(init_daily_routine(index*num_lifting_category, workout_program.training_day[index]))
         index += 1
@@ -42,6 +44,7 @@ def init_workout_program() :
 def init_lifting_set(position) :
 
     sheet = Scraper.bal_load_excel(EXCEL_SHEET_DIR)
+    
 
     lifting_set = ls.BALLiftingSet()
     lifting_set_data = Scraper.bal_get_lifting_set(sheet, position)
@@ -54,11 +57,13 @@ def init_lifting_set(position) :
 def init_daily_routine(position, day) :
     # using init_lifting_set(position)
     sheet = Scraper.bal_load_excel(EXCEL_SHEET_DIR)
+    num_lifting_category = len(Scraper.bal_get_lifting_category(sheet))
     
     
     daily_routine = dr.BALDailyRoutine()
     daily_routine.day = day
-    daily_routine.assistance_exercises = Scraper.bal_get_assistance_exercise(sheet, position/num_lifting_category)
+    daily_routine.assistance_exercises = Scraper.bal_get_assistance_exercise(sheet, int(position/num_lifting_category))
+    daily_routine_data = []
     for index in range(position, position + num_lifting_category):
         if init_lifting_set(index) is None:
             break
@@ -77,59 +82,3 @@ def init_daily_routine(position, day) :
 
 
 
-=======
-'''
-import Modules.Scraper.bal_excel_scraper as Scraper
-import Modules.Classes.bal_daily_routine as dr
-import Modules.Classes.bal_lifting_set as ls
-import Modules.Classes.bal_workout_program as wp
-'''
-
-from Modules.Scraper import bal_excel_scraper as Scraper
-from Modules.Classes import bal_daily_routine as dr
-from Modules.Classes import bal_lifting_set as ls
-from Modules.Classes import bal_workout_program as wp
-
-EXCEL_SHEET_DIR = "../test.xlsx"
-
-def init_workout_program() :
-    
-    sheet = Scraper.bal_load_excel(EXCEL_SHEET_DIR)
-    
-    workout_program = wp.BALWorkoutProgram(
-        Scraper.bal_get_author(sheet),
-        Scraper.bal_get_training_per_weeks(sheet),
-        Scraper.bal_get_weight_unit(sheet),
-        Scraper.bal_get_routine_name(sheet)
-    )
-
-    workout_program.training_day = Scraper.bal_get_training_days(sheet)
-
-    # 
-    # Additional Code here
-    #
-
-    return workout_program
-
-
-# RET : bal_lifting_set
-def init_lifting_set(position) :
-
-    sheet = Scraper.bal_load_excel(EXCEL_SHEET_DIR)
-
-    lifting_set = ls.BALLiftingSet()
-    lifting_set_data = Scraper.bal_get_lifting_set(sheet, position)
-    lifting_set.init_lifting_set(lifting_set_data[0], lifting_set_data[2], lifting_set_data[1])
-
-    return lifting_set
-
-
-# RET : bal_daily_routine
-def init_daily_routine(position) :
-    
-    # using init_lifting_set(position) 
-
-    pass
-
-
->>>>>>> Stashed changes
